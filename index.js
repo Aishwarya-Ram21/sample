2bc46c9c2a316600383d04aef6a27a5187e5281f
AkashB45 <akashbalaji594@gmail.com>
2024-07-06 13:09:04 +0530

    third

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const upload = multer();
 app.use(upload.fields([]));
 
 app.use(express.json());
+
+console.log('Hello World');
+app.get('/', (req, res)=>{
+    res.send('Hello World');
+});
 const port = 8081;
 app.listen(port, ()=>{
     console.log(`Listening at port ${port}`)
